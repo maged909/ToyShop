aba2ec3c421066f2d741d10d9e6d9a78474bb35c
maged909 <mkingm123@gmail.com>
2022-12-09 21:15:13 +0200

    'first-commit'

---
+++ script.js
@@ -0,0 +1,39 @@
+const submitContactBtn = document.getElementById("submitContact")
+const alertContactMessage = document.getElementById("alertContactMessage")
+const buyBtns = document.querySelectorAll(".buyBtn")
+const alertBaught = document.getElementById("alertBaught")
+const firstNameFeild = document.getElementById("firstName")
+
+submitContactBtn.addEventListener("click", alertSeccess);
+buyBtns.forEach(buyBtn => {
+    buyBtn.addEventListener("click", Baught);
+});
+var cart = 0;
+
+function alertSeccess() {
+    UserName = firstNameFeild.value
+    if(UserName == ''){
+        firstNameFeild.classList.add("border-danger")
+    }else{
+        firstNameFeild.classList.remove("border-danger")
+        alertContactMessage.classList.remove("d-none")
+
+        var inputs = document.querySelectorAll('.contactFormInput');
+        inputs.forEach(input => {
+            input.value='';
+        });
+
+        setTimeout(hideAlert, 5000);
+    }
+    alertContactMessage.innerHTML = 'Thank you '+UserName+' For Contacting us.'
+  }
+function hideAlert(){
+    alertContactMessage.classList.add("d-none")
+    alertBaught.classList.add("d-none")
+}
+function Baught(){
+    alertBaught.classList.remove("d-none");
+    cart = cart+1
+    alertBaught.innerHTML = 'Added To Cart - Your Cart Has '+cart+" element(s)!" 
+    setTimeout(hideAlert, 3000);
+}
